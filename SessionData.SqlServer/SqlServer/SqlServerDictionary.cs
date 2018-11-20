6eb99a8a151e8b33d8bf609c2faebb6d07df3dd9
Adam O'Neil <adamosoftware@gmail.com>
2018-11-20 08:37:57 -0500

    version 1.0.3 made backing table schema a variable, with default "session"

--- SessionData.SqlServer/SqlServer/SqlServerDictionary.cs
+++ SessionData.SqlServer/SqlServer/SqlServerDictionary.cs
@@ -8,40 +8,42 @@ namespace SessionData.SqlServer.SqlServer
 	public class SqlServerDictionary : DbDictionary
 	{
 		private readonly string _tableName;
+		private readonly string _schema;
 
-		public SqlServerDictionary(string connectionString, string tableName) : base(connectionString)
+		public SqlServerDictionary(string connectionString, string tableName, string schema = "session") : base(connectionString)
 		{
 			_tableName = tableName;
+			_schema = schema;
 		}
 
 		protected override bool IsInitialized => false;
 
 		protected override void Initialize(IDbConnection connection)
 		{
-			if (!SchemaExists(connection, "session"))
+			if (!SchemaExists(connection, _schema))
 			{
-				connection.Execute("CREATE SCHEMA [session]");
+				connection.Execute($"CREATE SCHEMA [{_schema}]");
 			}
 
-			if (!TableExists(connection, "session", _tableName))
+			if (!TableExists(connection, _schema, _tableName))
 			{
 				connection.Execute(
-					$@"CREATE TABLE [session].[{_tableName}] (
+					$@"CREATE TABLE [{_schema}].[{_tableName}] (
 						[Key] nvarchar(255) NOT NULL,
 						[Type] nvarchar(255) NOT NULL,
 						[Data] nvarchar(max) NOT NULL,
-						CONSTRAINT [PK_session_{_tableName}] PRIMARY KEY ([Key])
+						CONSTRAINT [PK_{_schema}_{_tableName}] PRIMARY KEY ([Key])
 					)");
 			}
 		}
 
-		protected override string QueryCommand => $"SELECT [Type], [Data] FROM [session].[{_tableName}] WHERE [Key]=@key";
+		protected override string QueryCommand => $"SELECT [Type], [Data] FROM [{_schema}].[{_tableName}] WHERE [Key]=@key";
 
-		protected override string InsertCommand => $"INSERT INTO [session].[{_tableName}] ([Key], [Type], [Data]) VALUES (@key, @type, @data)";
+		protected override string InsertCommand => $"INSERT INTO [{_schema}].[{_tableName}] ([Key], [Type], [Data]) VALUES (@key, @type, @data)";
 
-		protected override string UpdateCommand => $"UPDATE [session].[{_tableName}] SET [Type]=@type, [Data]=@data WHERE [Key]=@key";
+		protected override string UpdateCommand => $"UPDATE [{_schema}].[{_tableName}] SET [Type]=@type, [Data]=@data WHERE [Key]=@key";
 
-		protected override string DeleteCommand => $"DELETE [session].[{_tableName}] WHERE [Key]=@key";
+		protected override string DeleteCommand => $"DELETE [{_schema}].[{_tableName}] WHERE [Key]=@key";
 
 		protected override IDbConnection GetConnection()
 		{
@@ -50,7 +52,7 @@ namespace SessionData.SqlServer.SqlServer
 
 		protected override bool KeyExists(IDbConnection connection, string key)
 		{
-			return Exists(connection, $"SELECT 1 FROM [session].[{_tableName}] WHERE [Key]=@key", new { key = FormatKey(key) });
+			return Exists(connection, $"SELECT 1 FROM [{_schema}].[{_tableName}] WHERE [Key]=@key", new { key = FormatKey(key) });
 		}
 
 		private static bool Exists(IDbConnection connection, string query, object parameters)
